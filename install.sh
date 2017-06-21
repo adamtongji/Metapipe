3ed61a724d16c3135cd199620fdcb4388ecbb6de
adamtongji <adam.tongji@gmail.com>
2017-06-21 15:51:31 +0800

    meta_pipe

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-conda create -n metamed -c bioconda python=2.7 qiime matplotlib=1.4.3 mock nose bowtie2 samtools=1.4.1  
-# echo "source activate metamed" >> ~/.bashrc
-# source ~/.bashrc
+conda create -n metapipe -c bioconda python=2.7 qiime matplotlib=1.4.3 mock nose bowtie2 samtools=1.4.1  
